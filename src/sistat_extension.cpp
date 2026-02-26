120bcbb1e87b95c7b4bcd8d46eabf93f8447c179
fklezin <klezin.florijan95@gmail.com>
2026-02-26 11:12:04 +0100

    Make extension buildable on wasm by disabling OpenSSL path

--- src/sistat_extension.cpp
+++ src/sistat_extension.cpp
@@ -2,16 +2,22 @@
 
 #include "sistat_extension.hpp"
 #include "duckdb.hpp"
+#ifndef __EMSCRIPTEN__
 #include "sistat/sistat_data_functions.hpp"
 #include "sistat/sistat_info_functions.hpp"
+#endif
 
 #include "duckdb/main/extension/extension_loader.hpp"
 
 namespace duckdb {
 
 static void LoadInternal(ExtensionLoader &loader) {
+#ifndef __EMSCRIPTEN__
 	SistatDataFunctions::Register(loader);
 	SistatInfoFunctions::Register(loader);
+#else
+	(void)loader;
+#endif
 }
 
 void SistatExtension::Load(ExtensionLoader &db) {
