b7b2d13d8e327bd9b4ade5f9ea4b0f2173b2acbe
fklezin <klezin.florijan95@gmail.com>
2026-02-27 12:52:09 +0100

    Add automatic retry for transient extension build workflow failures

--- src/sistat_extension.cpp
+++ src/sistat_extension.cpp
@@ -2,16 +2,22 @@
 
 #include "sistat_extension.hpp"
 #include "duckdb.hpp"
+#ifndef __EMSCRIPTEN__
 #include "sistat/sistat_data_functions.hpp"
 #include "sistat/sistat_info_functions.hpp"
+#endif
 
 #include "duckdb/main/extension/extension_loader.hpp"
 
 namespace duckdb {
 
 static void LoadInternal(ExtensionLoader &loader) {
+#ifndef __EMSCRIPTEN__
 	SistatDataFunctions::Register(loader);
 	SistatInfoFunctions::Register(loader);
+#else
+	(void)loader;
+#endif
 }
 
 void SistatExtension::Load(ExtensionLoader &db) {
@@ -37,6 +43,10 @@ extern "C" {
 DUCKDB_EXTENSION_API void sistat_init(duckdb::DatabaseInstance &db) {
 	duckdb::DuckDB db_wrapper(db);
 	db_wrapper.LoadStaticExtension<duckdb::SistatExtension>();
+}
+
+DUCKDB_EXTENSION_API void sistat_duckdb_cpp_init(duckdb::DatabaseInstance &db) {
+	sistat_init(db);
 }
 
 DUCKDB_EXTENSION_API const char *sistat_version() {
